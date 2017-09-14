db6983f6d08d6b05509108402b63db8c65193c49
Guilherme Baldini <gbaldini@acessocard.com.br>
2017-09-14 12:19:50 -0300

    Tests and simple controller

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDocker.Controllers
+{
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+         [HttpGet]
+        public IActionResult Get(int IdArquivo)
+        {
+            return Ok(new List<string>(){
+               "Guilherme Baldini",
+               "João Salles",
+               "Anderson Vitorino" 
+            });
+        }
+    }
+}
